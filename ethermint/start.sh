bff4937b4bcf0b29f2a0e863adb9c0d7390144de
Alexander Lockshyn <info@lab37.com>
2018-01-24 19:38:17 +0200

    Moved ethereum monitoring out of main ethermint workers to separate containers

--- ethermint/start.sh
+++ ethermint/start.sh
@@ -3,18 +3,19 @@
 # Exit if any command return non-zero exitcode
 set -e
 
-if [ -n "${ALLOW_NETSTATS+set}" ]; then
-  if [ "$ALLOW_NETSTATS" == "1" ]; then
-    # Preparing eth-net-intelligence-api
-    perl -pi -e "s/XXX/$(hostname)/g" /root/eth-net-intelligence-api/app.json
+# TODO: uncomment if we'll use local monitors instead of external ones
+# if [ -n "${ALLOW_NETSTATS+set}" ]; then
+#   if [ "$ALLOW_NETSTATS" == "1" ]; then
+#     # Preparing eth-net-intelligence-api
+#     perl -pi -e "s/XXX/$(hostname)/g" /root/eth-net-intelligence-api/app.json
 
-    # Launching eth-net-intelligence-api
-    cd /root/eth-net-intelligence-api
-    /usr/bin/pm2 start ./app.json
-    cd ${DATA_ROOT}
-    sleep 3
-  fi
-fi
+#     # Launching eth-net-intelligence-api
+#     cd /root/eth-net-intelligence-api
+#     /usr/bin/pm2 start ./app.json
+#     cd ${DATA_ROOT}
+#     sleep 3
+#   fi
+# fi
 
 # Initialize working directory
 ethermint --datadir ${DATA_ROOT} init ${DATA_ROOT}/genesis.json
