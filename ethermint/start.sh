b91c66290e006168b91d38f86cdbf16ce85d136e
Alexander Lockshyn <info@lab37.com>
2018-01-26 11:36:59 +0200

    Integrating block explorer

--- ethermint/start.sh
+++ ethermint/start.sh
@@ -8,4 +8,4 @@ ethermint --datadir ${DATA_ROOT} init ${DATA_ROOT}/genesis.json
 sleep 3
 
 # Run Ethermint/Geth
-ethermint --tendermint_addr ${TENDERMINT_ADDR} --datadir ${DATA_ROOT} --rpc --rpcaddr=0.0.0.0 --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin
+ethermint --tendermint_addr ${TENDERMINT_ADDR} --datadir ${DATA_ROOT} --rpc --rpcaddr=0.0.0.0 --rpccorsdomain "*" --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin
